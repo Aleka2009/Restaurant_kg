cac26cdfcb753e5993ee68da6f30dc2a3a1ffbe8
Aleka <mergesevaalia@gmail.com>
2022-08-20 18:47:56 +0600

    fixed main urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls), # admin site
     path('auth/', include('custom_auth.urls')),
-    path('rest/', include('rest.urls')),
+    path('', include('rest.urls')),
     path('about_us/', include('about_us.urls')),
     path('silk/', include('silk.urls', namespace='silk')),
     path('i18n/', include('django.conf.urls.i18n')),
@@ -59,7 +59,7 @@ urlpatterns = [
 #     ]
 urlpatterns += i18n_patterns(
     path('auth/', include('custom_auth.urls')),
-    path('rest/', include('rest.urls')),
+    path('', include('rest.urls')),
     path('about_us/', include('about_us.urls')),
     prefix_default_language=False
 )
